1d1b9e4be238107a5b02f0cc3d6f6ebfb5638dd3
JMGQuery <138651894+JMGQuery@users.noreply.github.com>
2024-01-26 15:40:55 -0600

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,9 @@
+$(".main-contact .card-contact .site-cover").on("mouseover",function ()
+{
+$(this).css('transform', 'scale(110%)');
+})
+
+$(".main-contact .card-contact .site-cover").on("mouseout",function ()
+{
+$(this).css('transform', 'scale(100%)');
+})
